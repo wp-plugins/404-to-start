1ba21c32f93ec5dd52e9979f7fcd8464dbb4c60b
1manfactory <1manfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-10 19:21:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/404-to-start/trunk@343773 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 404-to-start.php
+++ 404-to-start.php
@@ -3,18 +3,130 @@
 Plugin Name: 404-to-start
 Plugin URI: http://1manfactory.com/4042start
 Description: Send every 404 page not found error directly to start page to overcome problems with search engines
-Version: 1.0
+Version: 1.1
 Author: J&uuml;rgen Schulze
 Author URI: http://1manfactory.com
+License: GNU GPL
 */
 
+/*  Copyright 2010 Juergen Schulze, 1manfactory.com (email : 1manfactory@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+f042start_set_lang_file();
+add_action('admin_init', 'f042start_register_settings' );
+add_action('admin_menu', 'f042start_plugin_admin_menu');
 add_action('template_redirect', 'f042start_output_header');
+register_activation_hook(__FILE__, 'f042start_activate');
+register_deactivation_hook(__FILE__, 'f042start_deactivate');
+register_uninstall_hook(__FILE__, 'f042start_uninstall');
+
+function f042start_register_settings() { // whitelist options
+  register_setting( 'f042start_option-group', 'f042start_type' );
+}
+
+function f042start_set_lang_file() {
+	# set the language file
+	$currentLocale = get_locale();
+	if(!empty($currentLocale)) {
+		$moFile = dirname(__FILE__) . "/lang/" . $currentLocale . ".mo";
+		if(@file_exists($moFile) && is_readable($moFile)) load_textdomain('f042start', $moFile);
+	}
+}
+
+function f042start_deactivate() {
+	// needed
+	delete_option('f042start_type');
+}
+
+function f042start_activate() {
+	# setting default values
+	add_option('f042start_type', '301');
+}
+
+
+function f042start_uninstall() {
+	# delete all data stored
+	delete_option('f042start_type');
+}
+
+function f042start_plugin_admin_menu() {
+	add_options_page(__('404 to Start Settings', 'f042start'), "404 to Start", 9, basename(__FILE__), 'f042start_plugin_options');
+}
+
+
+function f042start_plugin_options(){
+
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	echo '<div class="wrap">';
+
+
+	print '<h2>'.__('404 to Start Settings', 'f042start').'</h2>';
+
+	print '<form method="post" action="options.php" id="f042start_form">';
+	wp_nonce_field('update-options', '_wpnonce');
+	settings_fields( 'f042start_option-group' );
+
+	print'
+		<input type="hidden" name="page_options" value="f042start_type" />
+		<table class="form-table">
+		<tr valign="top">
+		<th scope="row">'.__('404 Redirect to start page', 'f042start').'</th>
+		</tr>
+		<tr>
+		<td>
+		
+		<input type="radio" name="f042start_type" value="off" '.f042start_checked("f042start_type", "off").'/> '.__('off', 'f042start').' <br />
+		<input type="radio" name="f042start_type" value="301" '.f042start_checked("f042start_type", "301").'/> '.__('301 - Moved permanently', 'f042start').'<br />
+		<input type="radio" name="f042start_type" value="302" '.f042start_checked("f042start_type", "302").'/> '.__('302 - Found/ Moved temporarily (not recommended)', 'f042start').'<br />
+		
+		</td>
+		</tr>
+		</table>
+		<input type="hidden" name="action" value="update" />
+
+	';
+
+
+	print '<p class="submit"><input type="submit" name="submit" value="'.__('Save Changes', 'f042start').'" /></p>';
+
+	print '</form>';
+	print '</div>';
+
+	print '<br /><br /><br />';
+	require_once('whatsup.php');
+	
+}
+
+
+function f042start_checked($checkOption, $checkValue) {
+	return get_option($checkOption)==$checkValue ? " checked" : "";
+}
+
 
 // Trap 404 errors and redirect them to start page
 // 301=permanently moved
+// 302=temporary
 function f042start_output_header() {
-	if ( !is_404() ) return;
-
-	wp_redirect( home_url(), 301 );
+	if ( !is_404() || get_option("f042start_type")=="off" ) return;
+	
+	#die (get_option("f042start_type"));
+	wp_redirect( home_url(), get_option("f042start_type") );
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -3,23 +3,24 @@ Contributors: 1manfactory
 Donate link: http://1manfactory.com/
 Tags: 404, start, start page, page not found, redirect, umleiten, startseite, fehler
 Requires at least: 2.8
-Tested up to: 3.0.4
+Tested up to: 3.0.5
 Stable tag: "trunk"
 
 Send every 404 page not found error directly to start page to overcome problems with search engines
 
 == Description ==
 
-If you don't like the typical 404 page than you can redirect all 404 page not found errors permantly to the start page.
+If you don't like the typical 404 page than you can redirect all 404 page not found errors permantly (or temporarily) to the start page.
 
 This will help you solve problems with external bad links which are not under your influence.
 
-Check out my other [Wordpress Plugins](http://wordpress.org/extend/plugins/profile/1manfactory)
+Chose between 301 - Moved permanently (default) and 302 - Moved temporarily.
 
 == Installation ==
 
 1. Upload 404-to-start to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Adjust settings
 done
 
 == Remove plugin ==
@@ -27,11 +28,17 @@ done
 1. Deactivate plugin through the 'Plugins' menu in WordPress
 2. Delete plugin through the 'Plugins' menu in WordPress
 
+It's best to use the build in delete function of wordpress. That way all the stored data will be removed and no orphaned data will stay.
+
+== Screenshots ==
+
+1. Basic Settings
+
 == Frequently Asked Questions ==
 
-= How does this plugin work? =
+= 301 or 302? =
 
-Just activate it.
+It is generally not recommended to use a 302 redirect unless you are a search engine expert and can make a unique redirect page for each search engine. Each of the three major search engines (Google, Yahoo and MSN) process 302 redirects in a different manner. [How to Write a 301 or 302 Website Redirect Using Php](http://www.ehow.com/how_2319373_write-301-302-website-redirect.html)
 
 = Is there anything I should be aware of? Any limitations?  =
 
@@ -39,7 +46,7 @@ No.
 
 = How can I support you? =
 
-Donation link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ESLEBR77J67A2
+Post a comment on [404-to-start](http://1manfactory.com/4042start)
 
 = What is the plugin page?  =
 
@@ -53,11 +60,16 @@ Check out my other [Wordpress Plugins](http://wordpress.org/extend/plugins/profi
 
 Post it at the plugin page: [404-to-start](http://1manfactory.com/4042start)
 
-== Screenshots ==
+== Upgrade Notice ==
 
-none
+Just do a normal upgrade.
 
 == Changelog ==
+
+= 1.1 (10.02.2010) =
+* new settings menu
+* chose between 301 and 302 redirect
+* tested with Wordpress 3.0.5
 
 = 1.0 (02.01.2010) =
 * first version
