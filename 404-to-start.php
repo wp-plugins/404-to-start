8ef871548ecb1a1da89f81e9fba3f85a1046934a
1manfactory <1manfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-13 07:21:44 +0000

    Better search enigine detection
    git-svn-id: https://plugins.svn.wordpress.org/404-to-start/trunk@712229 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 404-to-start.php
+++ 404-to-start.php
@@ -3,7 +3,7 @@
 Plugin Name: 404-to-start
 Plugin URI: http://1manfactory.com/4042start
 Description: Send 404 page not found error directly to start page (or any other page/site) to overcome problems with search engines. With optional email alert.
-Version: 1.5
+Version: 1.5.1
 Author: J&uuml;rgen Schulze
 Author URI: http://1manfactory.com
 License: GNU GPL
@@ -261,7 +261,7 @@ function f042start_curPageURL() {
 }
 
 function f042start_is_crawlers() {
-	$sites = 'Java|Google|Yahoo|Ask|bot'; // Add the rest of the search-engines 
+	$sites = 'crawler|Java|Google|Yahoo|Ask|bot'; // Add the rest of the search-engines 
 	return (preg_match("/$sites/i", $_SERVER['HTTP_USER_AGENT']) > 0) ? true : false;  	
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://1manfactory.com/donate
 Tags: 404, start, start page, page not found, redirect, umleiten, startseite, fehler
 Requires at least: 3.0
 Tested up to: 3.5.1
-Stable tag: 1.5
+Stable tag: 1.5.1
 
 Send 404 page not found error directly to start page (or any other page/site) to overcome problems with search engines. With optional email alert.
 
@@ -64,6 +64,9 @@ Post it at the plugin page: [404-to-start](http://1manfactory.com/4042start)
 Just do a normal upgrade.
 
 == Changelog ==
+
+= 1.5.1 (13.05.2013) =
+* Better search engine detection
 
 = 1.5 (30.03.2013) =
 * Bug fixing
