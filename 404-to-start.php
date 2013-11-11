ac27748a65431cf98919c8c45fa84b1875f43b3f
1manfactory <1manfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-11 09:08:32 +0000

    new search engines
    git-svn-id: https://plugins.svn.wordpress.org/404-to-start/trunk@802301 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 404-to-start.php
+++ 404-to-start.php
@@ -3,8 +3,8 @@
 Plugin Name: 404 to Start
 Plugin URI: http://1manfactory.com/4042start
 Description: Send 404 page not found error directly to start page (or any other page/site) to overcome problems with search engines. With optional email alert.
-Version: 1.5.5
-Author: J&uuml;rgen Schulze
+Version: 1.5.6
+Author: Jürgen Schulze
 Author URI: http://1manfactory.com
 License: GNU GPL
 */
@@ -261,7 +261,7 @@ function f042start_curPageURL() {
 }
 
 function f042start_is_crawlers() {
-	$sites = 'facebookexternalhit|butterfly|JS-Kit|InAGist|BUbiNG|crawler|Java|Google|Yahoo|Ask|bot|spider|Twikle|flipboard|longurl|crowsnest|peerindex|UnwindFetchor'; // Add the rest of the search-engines 
+	$sites = 'facebookexternalhit|genieo|butterfly|JS-Kit|InAGist|BUbiNG|crawler|Java|Google|Yahoo|Ask|bot|spider|Twikle|flipboard|longurl|crowsnest|peerindex|UnwindFetchor'; // Add the rest of the search-engines 
 	return (preg_match("/$sites/i", $_SERVER['HTTP_USER_AGENT']) > 0) ? true : false;  	
 }
 ?>
